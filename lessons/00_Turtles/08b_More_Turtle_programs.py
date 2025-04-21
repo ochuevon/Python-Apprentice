e5016a02981e54f9c1485718f7e06f22040e884c
ochuevon <129795360+ochuevon@users.noreply.github.com>
2025-04-21 23:56:57 +0000

    8 and almost done with 9

--- lessons/00_Turtles/08b_More_Turtle_programs.py
+++ lessons/00_Turtles/08b_More_Turtle_programs.py
@@ -8,3 +8,34 @@ Then change the code so that the turtle has a different image ( look in the 'ima
 directory ) and moves to the corners of the screen in a square pattern. 
 """
 
+import turtle as turtle
+
+def set_turtleimage(turtle, sanic_64):
+    """Set the turtles shape into a custom image."""
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / sanic_64)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('white')
+t = turtle.Turtle()
+set_turtleimage(t, "pikachu.gif")
+
+t.penup()
+t.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
+
+def turtle_clicked(t, x, y):
+
+    print('turtle clicked!')
+    
+    for i in range(0,360, 20):
+        t.tilt(20)
+
+t.onclick(lambda x, y, t=t: turtle_clicked(t, x, y))
+
+turtle.done() # Important! Use `done` not `exitonclick` to keep the window open
--- lessons/00_Turtles/09_LeagueBot.py
+++ lessons/00_Turtles/09_LeagueBot.py
@@ -11,10 +11,31 @@ Write your own turtle program! Here is what your program should do
 
 import turtle as turtle
 
+def set_turtle_image(turtle, image_name):
+    """Set the turtle's shape to a custom image."""
+
+    from pathlib import Path
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    screen = turtle.getscreen()
+    screen.addshape(image_path)
+    turtle.shape(image_path)
+set_turtle_image(turtle, "leaguebot_bolt.gif" )
+
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('white')
 
-t = turtle.Turtle()
+tina = turtle.Turtle()
 
-... # Your Code Here
+(width=10,height=10)
+
+tina.pencolor('blue')
+
+tina.speed(3)
+for i in range(8): 
+    tina.forward(120)
+    tina.left(45)
+
+turtle.exitonclick()
